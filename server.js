94b73181c884e155122de4cd00dd1f78186c78d1
Anisha <anishachowdhury34gmail.com>
2025-03-21 04:47:25 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Allow all origins (Change as needed)
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Socket.IO server running on port 3000");
+});
